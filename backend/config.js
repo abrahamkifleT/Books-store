6df36ee076e5a007fd9704902f6597a92ef2c37c
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-14 00:37:56 +0300

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 3000;
+export const mongoDBURL = `mongodb+srv://root:1234@books-store-mern.ipcygfx.mongodb.net/books-collecton?retryWrites=true&w=majority&appName=Books-Store-MERN`
