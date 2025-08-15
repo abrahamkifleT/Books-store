15a87309add3f47d05162ed0780126e1723f1ad5
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-15 17:23:22 +0300

    finish server side logic.

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./model/booksModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
 const app = express();
 
 //Middleware for parsing request body
@@ -10,6 +12,14 @@ app.use(express.json());
 
 //Middleware for handling routes
 app.use("/books", booksRoute);
+// Options 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+
+app.use(cors({
+    origin: "http//localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}))
 
 app.get("/", (req, res) => {
     return res.status(234).send("Welcome to mern stack");
