49b0124315a5e130ff6d03afcb7b34b1deaea487
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-15 18:55:57 +0300

    setting up front end development

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 3000;
+export const PORT = 5555;
 export const mongoDBURL = `mongodb+srv://root:1234@books-store-mern.ipcygfx.mongodb.net/books-collecton?retryWrites=true&w=majority&appName=Books-Store-MERN`
 
